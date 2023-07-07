3c6fce30484b561f9b6667608e53d7f9104ad3a3
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-07 15:53:27 -0300

    :lipstick: feat: globalstyles and themes

--- Profile/src/main.tsx
+++ Profile/src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global.ts";
+import { defaultTheme } from "./styles/themes/defaultTheme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
