bf9a3e234c112990bd0e32c4dfb4da1cbda3cdbd
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-11 10:22:27 -0300

    :sparkles: feat: useContext

--- Profile/src/main.tsx
+++ Profile/src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global.ts";
 import { defaultTheme } from "./styles/themes/defaultTheme.ts";
+import { SiderBarContext } from "./context/useContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <SiderBarContext>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </SiderBarContext>
   </React.StrictMode>
 );
